433627b8211557682eb81debe0b5cc922ebd25dd
Alice Freda <alicefreda@gmail.com>
2019-12-17 23:46:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,12 @@
 class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    attributes.each do |k, v|
+      self.send("k=", v)
+    end
+  end
+  
   def attributes
 
   end
