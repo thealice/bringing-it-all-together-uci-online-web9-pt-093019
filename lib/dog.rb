d44d4fce810cda4935bc8b9920366cffb7cd7432
Alice Freda <alicefreda@gmail.com>
2019-12-18 00:18:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,7 @@ class Dog
         DB[:conn].execute(sql, self.name, self.breed)
         self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    self
+    # self
   end
 
   def update
