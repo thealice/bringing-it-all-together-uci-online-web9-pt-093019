29e47eb4293c0ff66a2a82c366f606733df174f9
Alice Freda <alicefreda@gmail.com>
2019-12-18 00:05:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,7 @@ class Dog
           VALUES (?, ?)
         SQL
         DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     # end
   end
 
