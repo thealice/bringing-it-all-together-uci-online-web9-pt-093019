b00bfe5adec795af27801b2665e68839e09ff57e
Alice Freda <alicefreda@gmail.com>
2019-12-18 00:04:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,11 +24,15 @@ class Dog
   end
 
   def save
-    if self.id
-      self.update
-    else
-      
-    end
+    # if self.id
+    #   self.update
+    # else
+        sql = <<-SQL
+          INSERT INTO dogs
+          VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+    # end
   end
 
 end
