38a73acf1fec6f44192c6f6a5e9caa7a79b9c86e
Alice Freda <alicefreda@gmail.com>
2019-12-18 00:08:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,4 +36,9 @@ class Dog
     end
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
